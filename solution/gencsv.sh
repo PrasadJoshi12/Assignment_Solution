381ff5bd77af403ca5ae083c1ac7ec623caf1535
prash312 <p.joshi86753@gmail.com>
2021-01-20 23:43:38 +0530

    Update gencsv.sh

--- solution/gencsv.sh
+++ solution/gencsv.sh
@@ -3,12 +3,12 @@ if [ $# -eq 0 ]
 then
         for num in {0..9}
         do
-                echo "$num, $RANDOM" >> inputdata
+                echo "$num, $RANDOM" >> inputFile
         done
 else
         N=$1
         for ((i=0 ; i < N ; i++))
         do
-                echo "$i, $RANDOM" >> inputdata
+                echo "$i, $RANDOM" >> inputFile
         done
 fi
